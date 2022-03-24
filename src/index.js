72970528d727e6d3408458f027a25a51f820435c
Danilo luizy <nillomalla19@hmail.com>
2022-03-24 11:57:36 -0300

    atualizando projeto

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CounterContextProvider } from './contexts/CounterContext';
 import './Styles/global-styles.css';
 
 import { Home } from './templates/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <CounterContextProvider>
+      <Home />
+    </CounterContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
